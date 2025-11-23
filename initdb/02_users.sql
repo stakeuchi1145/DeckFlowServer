22558620f5fc154365010c8aae5a02117044e18b
stakeuchi1145 <shintyakos@engineer-life.com>
2025-11-23 21:08:40 +0900

    UID変更

--- initdb/02_users.sql
+++ initdb/02_users.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (display_name, email, auth_provider, auth_uid, created_at, updated_at)
 VALUES
-    ('Taro', 'taro@example.com', 'firebase', 'YW8U1iLCjUhevwUMzSPHAJV0FVk2', NOW(), NOW())
+    ('Taro', 'taro@example.com', 'firebase', 'sX4AGBL23Pc6ldlQ0TrIyxGXxFE2', NOW(), NOW())
